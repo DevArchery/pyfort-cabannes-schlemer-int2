68a38fef10d80eca5740b9fccf96ef63b67106df
DevArchery <valentin.cabannes@efrei.net>
2024-12-01 20:01:51 +0100

    Gambling patch
    Finished work on the luck based challenges in chance_challenges.py

--- chance_challenges.py
+++ chance_challenges.py
@@ -2,8 +2,9 @@
 ##imports
 import random as rnd
 import toolbox as tools
+
 ##script
-def shell_game(nb=3):
+def shell_game(nb=3)->bool:
     """Implement the shell game, where the player has to guess which shell contains the key. nb is the number of shells
     and is set to 3 by default"""
     print("A key is hidden under one of three shells. Guess which shell it is in 2 tries or less to get it")
@@ -27,3 +28,23 @@ def shell_game(nb=3):
             guess=None
     print("You lost! The key was under the shell", key)
     return False
+
+def roll_dice_game(nbthrows)->bool:
+    throws,finished=1,False
+    while throws<=nbthrows:
+        player, game_master = (rnd.randint(1, 6), rnd.randint(1, 6)) , (rnd.randint(1, 6), rnd.randint(1, 6))
+        for i in range(2):
+            if player[i]==6 and game_master[i]!=6:
+                print("You won!")
+                return True
+            elif player[i]!=6 and game_master[i]==6:
+                print("You lost!")
+                return False
+            else:
+                print("It's a draw! Let's roll again!")
+        throws+=1
+    print("The game master won by default!")
+    return False
+def chance_challenges():
+    challenges=[shell_game(rnd.randint(3,5)), roll_dice_game(rnd.randint(3,5))]
+    return rnd.choice(challenges)
