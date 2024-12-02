4595b84cc7736b473be6c4218759d87270bf0bb2
Hugo Schlemer <hugo.schlemer@efrei.net>
2024-12-02 09:36:09 +0100

    logical update
    not the final version but the main body of the game

---
+++ logical_challenges.py
@@ -0,0 +1,40 @@
+<<<<<<< Updated upstream
+=======
+
+
+global x
+global n
+n = 0
+
+def display_sticks(x):
+    for i in range(x):
+        print("|", end="")
+
+def player_removal(x):
+    n = x
+    try:
+        nb = 5
+        while nb > 3:
+            print("How many sticks do you wanna remove ?")
+            nb = int(input())
+            n -= nb
+        display_sticks(x)
+    except ValueError:
+        print("Please enter a valid number")
+    except TypeError:
+        print("Please enter a valid number")
+    master_removal(n)
+    return n
+
+def master_removal(n):
+    n=int(4-player_removal(x))
+    for i in range(n):
+        print("|", end="")
+
+x = int(input("Enter the number of sticks : "))
+
+display_sticks(x)
+player_removal(x)
+
+
+>>>>>>> Stashed changes
