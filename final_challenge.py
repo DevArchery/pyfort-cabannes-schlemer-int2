cb99c99f77a6e68d0183fc1bcf2ac15eabc7827a
DevArchery <valentin.cabannes@efrei.net>
2024-12-04 19:12:15 +0100

    final_challenge.py
    Adds most of final_challenges

---
+++ final_challenge.py
@@ -0,0 +1,34 @@
+import json
+from toolbox import uppercase
+from random import randint,choice
+def treasure_room():
+    with open('data/TRClues.json') as f:
+        data = json.load(f)
+    year=str(randint(2015,2019))
+    shows=list(data['Fort Boyard'][year].keys())
+    show=choice(shows)
+    chosen=data['Fort Boyard'][year][show]
+    print(chosen)
+    for i in range(1,4):
+        print(f"Clue {i}:"+chosen['Clues'][i])
+    j=3
+    for i in range(4,7):
+        guess=uppercase(str(input("What is your answer? ")))
+        if guess==chosen['CODE-WORD']:
+            print("Congratulations! You have found the treasure.")
+            return True
+        else:
+            k=0
+            j+=1
+            while k<j:
+                print(f"Clue {k}:"+chosen['Clues'][k])
+                k+=1
+    guess=uppercase(str(input("What is your answer? ")))
+    if guess==chosen['CODE-WORD']:
+        print("Congratulations! You have found the treasure.")
+        return True
+    else:
+        print("You have failed to find the treasure.")
+        return False
+
+treasure_room()
